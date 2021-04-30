b299632fb91ca12f5e3df606e3f6201e4c207ddb
LucaDiBona <76700950+LucaDiBona@users.noreply.github.com>
2021-04-30 20:41:03 +0100

    determined structure attribute

--- latexFile.py
+++ latexFile.py
@@ -2,15 +2,26 @@ class LatexFile():
 
     def __init__(self, fileName: str) -> None:
         self.__STRUCTURE_LAYER = {"title": None, "subSections": []}
-        self.SECTION_HIERARCHY = ["chapter","section","subsection","subsubsection"]
+        self.SECTION_HIERARCHY = ["chapter",
+                                  "section", "subsection", "subsubsection"]
         self.fileName = fileName
         self.__f = open(fileName, "a+")
         self.__f.seek(0)
         self.__fContentsI = self.__f.read()
-        self.structure = []  # structure containing a list of dictionaries, each with the title as a string and a list of dictionaries of the next layer
-        for i, val in enumerate(self.SECTION_HIERARCHY):
-            for j in range(len(self.__fContentsI)):
 
+        # lists all sections and their positions in the form {<layer>,<title>,<position>}
+        self.structure = []
+        for i, val in enumerate(self.SECTION_HIERARCHY):
+            j = 0
+            # iterates through current string, finding all subsections
+            while j <= len(self.__fContentsI):
+                self.currentDir = self.inBrackets(val, j)
+                if self.currentDir["endPos"] >= 0:
+                    self.structure.append(
+                        {"layer": i, "title": self.currentDir["contents"], "pos": self.currentDir["pos"]})
+                    j = self.currentDir["endPos"]
+                else:
+                    j += 1
 
     def inBrackets(self, keyword: str, startPos: int = 0) -> dict:
         """Finds words inside {} for next appropriate LaTeX command
@@ -20,7 +31,11 @@ class LatexFile():
             startPos (int): the initial position to search from, defaults to 0
 
         Returns:
-            dict: the position of the '{' as pos, the position of the '}' as endPos and the contents of the {} as contents
+            dict: {
+                pos (int): the position of the '{'
+                endPos (int): the position of the '}'
+                contents (str): the string between the '{' and the '}'
+                }
         """
         self.__pos = self.__fContentsI.find(
             ("\\"+keyword + "{"), startPos, len(self.__fContentsI))
@@ -37,7 +52,7 @@ class LatexFile():
             for i in range((self.__pos + len(keyword) + 2), len(self.__fContentsI)):
                 if self.__fContentsI[i] == "}" and self.__unpairedBracketCount == 0:
                     self.__endPos = self.__pos + len(self.__bracketContents)
-                    self.__endPos +=1
+                    self.__endPos += 1
                     return({
                         "pos": self.__pos,
                         "endPos": self.__endPos,
@@ -52,6 +67,5 @@ class LatexFile():
                     self.__bracketContents += self.__fContentsI[i]
 
             # Error raised if unpaired '{' or '}' present after startPos
-            raise TypeError("Incorrectly formatted .tex document or misaligned startPos")
-
-    def getSection(self):
+            raise TypeError(
+                "Incorrectly formatted .tex document or misaligned startPos")
